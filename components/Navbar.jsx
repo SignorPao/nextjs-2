82fdb7ebfa2032bcbd6d7805021a28d40541a66a
Pavel Belousov <signorpao@gmail.com>
2023-03-30 17:43:08 +0300

    ...nav...

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,54 +1,61 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 import logoImg from "@/public/PB.png";
 
+import { navData } from "@/data/navData";
+
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const [navBg, setNavBg] = useState("transparent");
+  const [navTxt, setNavTxt] = useState("white");
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
-    <nav className="font-inter fixed left-0 top-0 w-full z-10 ease-in duration-300">
+    <nav style={{backgroundColor: `${navBg}`}} className="font-inter fixed left-0 top-0 w-full z-10 ease-in duration-300">
       <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
         <Link href="/" className="z-10">
           <Image src={logoImg} width={40} height={30} alt="logo" />
         </Link>
 
         <ul className="hidden sm:flex gap-x-4">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/#gallery">Gallery</Link>
-          </li>
-          <li>
-            <Link href="/portfolio">Work</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
+          {navData.map(({ id, link, href }) => (
+            <li key={id}>
+              <Link href={href} className="navLink">
+                {link}
+              </Link>
+            </li>
+          ))}
         </ul>
 
         {/* mobile btn */}
         <div className="block sm:hidden z-10">
-          <AiOutlineMenu size={20} />
+          {nav ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
+          {/* <AiOutlineMenu size={20} /> */}
         </div>
 
         {/* mobile menu */}
-        <div className="sm:hidden absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center w-full h-screen bg-black/80 text-center ease-in duration-300">
-          <ul className="flex flex-col gap-y-4 text-4xl">
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/#gallery">Gallery</Link>
-            </li>
-            <li>
-              <Link href="/portfolio">Work</Link>
-            </li>
-            <li>
-              <Link href="/contact">Contact</Link>
-            </li>
+        <div
+          className={
+            nav
+              ? "sm:hidden absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center w-full h-screen bg-black/90 text-center ease-in duration-300"
+              : "sm:hidden absolute top-0 bottom-0 -left-[100%] right-0 flex items-center justify-center w-full h-screen bg-black/90 text-center ease-in duration-300"
+          }
+        >
+          <ul className="flex flex-col gap-y-4">
+            {navData.map(({ id, link, href }) => (
+              <li key={id}>
+                <Link href={href} className="navLink text-4xl">
+                  {link}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
---
+++ data/navData.js
@@ -0,0 +1,22 @@
+export const navData = [
+  {
+    id: 1,
+    link: "Home",
+    href: "/",
+  },
+  {
+    id: 2,
+    link: "Gallery",
+    href: "/#gallery",
+  },
+  {
+    id: 3,
+    link: "Work",
+    href: "/portfolio",
+  },
+  {
+    id: 4,
+    link: "Contact",
+    href: "/contact",
+  },
+];
